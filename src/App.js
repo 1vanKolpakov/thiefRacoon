ed1930840a5892cf83d092327d6154fb85971af8
Ivan Kolpakov <vanjaklp95@gmail.com>
2026-05-24 20:10:27 +0300

    test subdomain deploy

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
   return (
     <div className="App">
       <h1>Распределение шаров по мешку</h1>
-      <p>test of deploy</p>
+      <p>test subdomain deploy</p>
       <div>
         <label>Емкость мешка: </label>
         <input
